9febc2ba58dc1f4a0ff64df112d7e7440acf0e21
rajmishra-47 <81914386+rajmishra-47@users.noreply.github.com>
2022-08-15 09:40:27 +0530

    basic changes

---
+++ src/components/C1.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function C1() {
+  return (
+    <>
+    <h1>Our Awesome Sponsors</h1>
+    <h4>Our love for open source and drive to make an impact materialized as HackNITR 3.0. Backed by likes of participating students, we grew steadily to organize hackathons and support social initiatives. Today,we bring you the opportunity to collaborate with us on our next endeavour. <a href="">Sponsor us</a></h4>
+    </>
+  )
+}
---
+++ src/components/box1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Box1() {
+  return (
+    <>
+    <div className="b1">
+        <span>ALPHA SPONSOR</span>
+        
+    </div>
+    </>
+  )
+}
